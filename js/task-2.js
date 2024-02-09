484f9a9cf10c8e775a8b6776d391d95a68048f90
Waffentracher <142826622+Waffentracher@users.noreply.github.com>
2024-02-09 21:09:38 +0200

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,9 +1,9 @@
 function formatMessage(message, maxLength) {
     if (message.length <= maxLength) {
-        return message
+        return message;
     }
     else {
-        return message.slice(0, maxLength) + "..."
+        return message.slice(0, maxLength) + "...";
         
     }
        
